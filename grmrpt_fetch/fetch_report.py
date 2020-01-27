93b7cf0a0fb5fa87fd337e20daead15bc93ed616
alexphi <alexphi@umich.edu>
2020-01-27 14:30:25 -0700

    Updated settings.py to allow hosting at bluemoongroom.com and setup HTTPS certificate

--- grmrpt_fetch/fetch_report.py
+++ grmrpt_fetch/fetch_report.py
@@ -33,28 +33,28 @@ if __name__ == "__main__":
     resorts = get_api('resorts/', headers={'Authorization': 'Token {}'.format(TOKEN)}, api_url=API_URL)
 
     # Fetch grooming report for each resort
-    for resort_dict in resorts:
-        resort = resort_dict['name']
-
-        report_url = resort_dict['report_url']
-        parse_mode = resort_dict['parse_mode']
-
-        if parse_mode == 'json':
-            response = requests.get(report_url)
-            if response.status_code != 200:
-                raise ValueError('Unable to fetch grooming report: {}'.format(response.text))
-
-            date, groomed_runs = get_grooming_report(parse_mode, response=response)
-        else:
-            date, groomed_runs = get_grooming_report(parse_mode, url=report_url)
-
-        create_report(date, groomed_runs, resort_dict['id'], API_URL, TOKEN, requests, get_api)
+    # for resort_dict in resorts:
+    #     resort = resort_dict['name']
+    #
+    #     report_url = resort_dict['report_url']
+    #     parse_mode = resort_dict['parse_mode']
+    #
+    #     if parse_mode == 'json':
+    #         response = requests.get(report_url)
+    #         if response.status_code != 200:
+    #             raise ValueError('Unable to fetch grooming report: {}'.format(response.text))
+    #
+    #         date, groomed_runs = get_grooming_report(parse_mode, response=response)
+    #     else:
+    #         date, groomed_runs = get_grooming_report(parse_mode, url=report_url)
+    #
+    #     create_report(date, groomed_runs, resort_dict['id'], API_URL, TOKEN, requests, get_api)
 
     # Check for notif
     get_api_wrapper = lambda x: get_api(x, headers={'Authorization': 'Token {}'.format(TOKEN)},
                                         api_url=API_URL)
-    resort_list = get_resorts_to_notify(get_api_wrapper, API_URL)
+    # resort_list = get_resorts_to_notify(get_api_wrapper, API_URL)
     # post_messages(resort_list, headers={'Authorization': 'Token {}'.format(TOKEN)}, api_url=API_URL)
-    # post_messages(['http://dev-env.exm5cdp7tw.us-west-2.elasticbeanstalk.com/bmreports/29/'],
-    #               headers={'Authorization': 'Token {}'.format(TOKEN)}, api_url=API_URL)
+    post_messages(['http://dev-env.exm5cdp7tw.us-west-2.elasticbeanstalk.com/bmreports/54/'],
+                  headers={'Authorization': 'Token {}'.format(TOKEN)}, api_url=API_URL)
     FOO = 1
--- grmrpt_site/grmrptcore/settings.py
+++ grmrpt_site/grmrptcore/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'dev-env.exm5cdp7tw.us-west-2.elasticbeanstalk.com',
-    '127.0.0.1'
+    '127.0.0.1',
+    'bluemoongroom.com'
 ]
 
 
